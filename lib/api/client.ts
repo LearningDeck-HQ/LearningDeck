852ba0d8f61ba654e5a924e2c5c01aa45ae74d31
ProdevappOFFICIAL <122856789+ProdevappOFFICIAL@users.noreply.github.com>
2026-04-25 17:53:07 +0100

    added login

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,6 +1,6 @@
 import { ApiResponse } from "@/types";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
 
 const getHeaders = () => {
   const headers: Record<string, string> = {
